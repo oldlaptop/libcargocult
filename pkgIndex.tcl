1c0ef653375d0b6ff8feae2951a8646fb4614133
oldlaptop <peterjpiwowarski@gmail.com>
2021-09-07 11:36:27 -0400

    regular-polygon procedure; first non-auto-culted addition!

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -2,6 +2,7 @@ eval [pkg::create -name cargocult -version 0.2 \
 	-source cargocult.tcl \
 	-source db.tcl \
 	-source io.tcl \
+	-source math.tcl \
 	-source meta.tcl]
 
 eval [pkg::create -name cargocult::tk -version 0.1 \
