f94bb5d8b94d6ae96cd82d62f9b8b9759e06c9a3
Peter Piwowarski <peterjpiwowarski@gmail.com>
2022-05-24 15:56:53 -0400

    TDBC statement cache
    One day, manual prepared statement management will be consigned to the
    ash heap of history!

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -4,7 +4,8 @@ eval [pkg::create -name cargocult -version 0.4 \
 	-source fs.tcl \
 	-source io.tcl \
 	-source math.tcl \
-	-source meta.tcl]
+	-source meta.tcl \
+	-source tdbc.tcl]
 
 eval [pkg::create -name cargocult::tk -version 0.1 \
 	-source tk.tcl]
---
+++ tdbc.tcl
@@ -0,0 +1,65 @@
+# TDBC-related cargo.
+
+package require tdbc
+
+namespace eval cargocult {
+
+# SQLite-style prepared statement cache for TDBC. The constructor expects an
+# open tdbc::connection handle. The primary entry point (as for SQLite) is the
+# eval method, which functions vaguely like SQLite's [$db eval]. No interface
+# for managing the cache manually yet exists; for example, if some outside force
+# closes its prepared statements, or the database connection goes away, or some
+# other disaster occurs, the only recourse is to destroy and recreate the cache.
+oo::class create statementcache {
+	variable connection
+	variable statements
+
+	constructor {new_connection} {
+		set connection $new_connection
+		set statements [dict create]
+	}
+
+	# Closes any prepared statements managed by the cache, but leaves the
+	# connection intact.
+	destructor {
+		if {[info exists statements]} {
+			foreach obj [dict values $statements] {
+				$obj close
+			}
+		}
+	}
+
+	# Evaluates $sql against the database, through the cache of prepared
+	# statements. If $sql has never been evaluated through this method
+	# on this cache object before, a statement is prepared against the
+	# database with that body and added to the cache. If it *has* been
+	# evaluated before, a statement will be in the cache, and will be used
+	# instead of preparing a new one.
+	#
+	# If no arguments other than $sql are passed, returns the result of
+	# calling the prepared statement's allrows method in the caller's scope.
+	# Otherwise, returns the result of calling the statement's foreach
+	# method, passing all arguments to it unchanged.
+	method eval {sql args} {
+		if {![dict exists $statements $sql]} {
+			dict set statements $sql [$connection prepare $sql]
+		}
+		if {[llength $args] == 0} {
+			uplevel 1 [list [dict get $statements $sql] allrows]
+		} else {
+			uplevel 1 [list [dict get $statements $sql] foreach {*}$args]
+		}
+	}
+
+	# Closes the associated database connection and destroys the cache.
+	method close {} {
+		$connection close
+		# Statements have been cleaned up, we can fry the references...
+		set statements {}
+
+		# ...so the destructor won't try to destroy them itself.
+		my destroy
+	}
+}
+
+} ;# namespace eval cargocult
